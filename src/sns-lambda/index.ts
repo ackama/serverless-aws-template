d309d544aa989dba506431df0a1d4766516e3c4a
Gareth Jones <jones258@gmail.com>
2024-06-16 07:53:30 +1200

    feat: update everything to latest minor versions

--- src/sns-lambda/index.ts
+++ src/sns-lambda/index.ts
@@ -10,7 +10,7 @@ const buildNotificationMessage = (sns: SNSMessage): string =>
     'You have a new notification:',
     sns.Timestamp,
     '',
-    `*${sns.Subject}*`,
+    `*${sns.Subject as string}*`,
     '',
     sns.Message
   ].join('\n');
